1c5e111b5636f44ca76777fa49fedf625ee15e09
wkdxo <wkdxo@ts>
2021-12-13 19:23:30 +0900

    버전 4.0 변경점 : TestJava2 변경

--- Test826/src/TestJava.java
+++ Test826/src/TestJava.java
@@ -3,7 +3,7 @@ public class TestJava {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!!");
-		System.out.println("ver : 3.0");
+		System.out.println("ver : 4.0");
 		System.out.println("로그인 완료 및 인코딩");
 	}
 }
