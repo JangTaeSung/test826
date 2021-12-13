5a8c37ad8334df6fe2c8bdcf1314138dc78ccc21
wkdxo <wkdxo@ts>
2021-12-13 18:17:42 +0900

    버전 2.0 로그인 완료

--- Test826/src/TestJava.java
+++ Test826/src/TestJava.java
@@ -2,7 +2,8 @@
 public class TestJava {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git");
-		System.out.println("ver : 1.0");
+		System.out.println("Hello Git!!!");
+		System.out.println("ver : 2.0");
+		System.out.println("로그인 완료");
 	}
 }
