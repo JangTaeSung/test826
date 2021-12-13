2146aec5ac35951c103aaf87da82c48039b8097e
wkdxo <wkdxo@ts>
2021-12-13 19:25:44 +0900

    버전 5.0 또또 업데이트

--- Test826/src/TestJava.java
+++ Test826/src/TestJava.java
@@ -3,7 +3,7 @@ public class TestJava {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!!");
-		System.out.println("ver : 4.0");
-		System.out.println("로그인 완료 및 인코딩");
+		System.out.println("ver : 5.0");
+		System.out.println("로그인 완료 및 인코딩 기능 업그레이드");
 	}
 }
